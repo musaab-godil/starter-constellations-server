7bbfb48843489a7f72fedb055404f4db295ce2fb
Musaab Godil <me@musaab.io>
2021-03-09 20:09:44 -0500

    Created inspect.js
    Reviewing basics of Promise chaining

--- requests-in-javascript/requests.js
+++ requests-in-javascript/requests.js
@@ -14,13 +14,13 @@ const url = "http://localhost:5000/constellations";
 //         console.log(error.message);
 //     });
 
-const constBody = {
-  name: "Ara",
-  meaning: "Altar",
-  starsWithPlanets: 7,
-  quadrant: "SQ3",
-  id: "DVaSPTf",
-};
+// const constBody = {
+//   name: "Ara",
+//   meaning: "Altar",
+//   starsWithPlanets: 7,
+//   quadrant: "SQ3",
+//   id: "DVaSPTf",
+// };
 
 // axios
 //     .post(url, constBody)
@@ -35,9 +35,46 @@ const constBody = {
 //         console.log(response.data);
 //     });
 
-axios
-    .get(url)
-    .then( (response) => {
-        console.log(response.status);
-        console.log(response.data);
-    });
+// axios
+//     .get(url)
+//     .then( (response) => {
+//         console.log(response.status);
+//         console.log(response.data);
+//     });
+
+
+const BASE_URL = "http://localhost:5000";
+const constellationsUrl = `${BASE_URL}/constellations`;
+
+const leo = {
+    name: "Leo",
+    meaning: "Lion",
+    starsWithPlanets: 19,
+    quadrant: "NQ2",
+};
+
+// axios
+//   .get(constellationsUrl)
+//   .then(({ data }) => {
+//     const exists = data.find(({ name }) => name === leo.name);
+//     if (exists) throw `Constellation "${leo.name}" already exists.`;
+//     return axios
+//       .post(constellationsUrl, leo)
+//       .then(({ data }) => console.log(data));
+//   })
+//   .catch(console.log);
+
+
+  axios
+  .get(constellationsUrl)
+  .then(({ data }) => {
+    return data.find(({ name }) => name === leo.name);
+  })
+  .then((exists) => {
+    if (exists) throw `Constellation "${leo.name}" already exists.`;
+    return axios.post(constellationsUrl, leo);
+  })
+  .then(({ data }) => console.log(data))
+  .catch(console.log);
+
+  // Flatter and cleaner version
